2b205bcb89ad483cd5ee499534ac2f1c2a1bf45a
ehpc <ehpc@ehpc.io>
2020-06-19 15:34:35 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Web Components example
+
+This repo contains a real-time clock encapsulated inside Web Component.
+
+See `src/index.html` for details.
---
+++ src/realtime-clock.js
@@ -0,0 +1,22 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<strong><slot></slot></strong>
+<span class="time"></span>
+`;
+
+class Clock extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' })
+      .appendChild(template.content.cloneNode(true));
+    this.runClock();
+  }
+
+  runClock() {
+    const timeEl = this.shadowRoot.querySelector('.time');
+    timeEl.textContent = new Date().toLocaleString();
+    setTimeout(this.runClock.bind(this), 1000);
+  }
+}
+
+customElements.define('realtime-clock', Clock);
